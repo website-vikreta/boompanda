a6636969cb0f5b7c40b17e5bb30f3e37415c925f
sbgprojects <shubham.gujarathi@decartem.com>
2020-11-18 18:33:09 +0530

    form designed

--- dashboard/js/main.js
+++ dashboard/js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
 
+    //checkbox unchecked
+    $(".toggle-check").change(function () {
+        $(".toggle-check").not(this).prop('checked', false);
+    });
     // navbar toggle
     $("#toggle").on('click', function () {
         $(".vertical-nav").toggleClass('show');
